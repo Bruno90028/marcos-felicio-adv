8226597ce90deaa03d6f08df8e08450d598a852e
Bruno90028 <bruno.camacedo.2017@gmail.com>
2024-09-02 17:49:25 -0300

    atuação

---
+++ script/atuacao.js
@@ -0,0 +1,69 @@
+function civil() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Cível (ou Direito Civil):'
+    descricao.innerText = 'O direito cível é uma área ampla que regula as relações entre pessoas físicas e jurídicas no dia a dia. Ele abrange uma série de subáreas, como obrigações, contratos, responsabilidade civil, direito das coisas, família e sucessões. O objetivo do direito cível é estabelecer regras para garantir que essas interações sejam justas e equilibradas, protegendo os direitos individuais e coletivos.'
+    exemplo.innerText = 'Imagine que você está caminhando na calçada e uma pessoa andando de bicicleta esbarra em você, causando uma lesão. Você pode usar o direito cível para processar essa pessoa por danos morais e materiais, buscando uma compensação financeira pelo ocorrido.'
+}
+
+function contratos() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Contratos'
+    descricao.innerText = 'Essa área é especializada em acordos entre duas ou mais partes que criam obrigações legais. O direito dos contratos regula a formação, execução e rescisão desses acordos. Ele garante que as partes cumpram o que foi combinado e prevê soluções para o caso de descumprimento, como a rescisão do contrato ou indenização por danos.'
+    exemplo.innerText = 'Se você contratar uma empresa para reformar sua casa e eles não cumprirem o prazo ou a qualidade acordada no contrato, você pode exigir que cumpram o combinado ou buscar uma compensação pelos danos causados.'
+}
+
+function consumidor() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Consumidor'
+    descricao.innerText = 'O direito do consumidor é focado na proteção dos consumidores contra práticas abusivas por parte de fornecedores de produtos e serviços. Ele regula as relações de consumo para garantir que os direitos dos consumidores sejam respeitados, como a qualidade dos produtos, a veracidade das informações, e a segurança dos serviços prestados.'
+    exemplo.innerText = 'Se você comprar um produto eletrônico que quebra poucos dias após a compra e a loja se recusar a consertar ou trocar, você pode usar o direito do consumidor para exigir seus direitos, como o reparo do produto ou a devolução do valor pago.'
+}
+
+function criminal() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Criminal'
+    descricao.innerText = 'O direito criminal trata de crimes, que são ações ou omissões que a lei define como infrações penais, e das sanções aplicáveis a quem os comete. Essa área envolve o processo penal, desde a investigação policial até o julgamento e a aplicação de penas. O objetivo do direito criminal é proteger a sociedade, punir os infratores e prevenir novos crimes.'
+    exemplo.innerText = 'Em um caso de roubo de carro, o direito criminal envolve a investigação pela polícia, o julgamento do suspeito e, se condenado, a aplicação de uma pena, que pode ser prisão, multa ou outras medidas.'
+}
+
+function familia() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Família'
+    descricao.innerText = 'O direito de família regula as relações familiares e aborda questões como casamento, união estável, divórcio, guarda de filhos, adoção, pensão alimentícia, e reconhecimento de paternidade. Ele busca proteger os direitos e interesses de todos os membros da família, especialmente os mais vulneráveis, como crianças e idosos.'
+    exemplo.innerText = 'Em um caso de divórcio, o direito de família determina como os bens do casal serão divididos, quem ficará com a guarda dos filhos e como será estipulada a pensão alimentícia.'
+}
+
+function imobiliario() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Imobiliário'
+    descricao.innerText = 'O direito imobiliário trata das questões relacionadas a imóveis, incluindo compra e venda de propriedades, contratos de locação, usucapião (direito de adquirir um bem por posse prolongada), e regularização de escrituras. Essa área também lida com a regulamentação de condomínios e a resolução de disputas de posse ou propriedade.'
+    exemplo.innerText = 'Se você compra um apartamento e, posteriormente, descobre que existem pendências de regularização com a prefeitura, o direito imobiliário fornece os mecanismos legais para resolver essas questões e garantir seu direito de propriedade.'
+}
+
+function militar() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+
+    titulo.innerText = 'Militar'
+    descricao.innerText = 'O direito militar se aplica especificamente às Forças Armadas (Exército, Marinha e Aeronáutica) e às Forças Auxiliares (Polícia Militar e Corpo de Bombeiros). Regula questões disciplinares, hierárquicas, e administrativas internas das instituições militares, além de tratar de crimes militares, que são infrações cometidas por militares em serviço ou em função da sua posição.'
+    exemplo.innerText = 'Se um soldado for acusado de desobediência grave a ordens superiores durante uma missão, ele será julgado de acordo com o código de justiça militar, que pode aplicar penas que variam de advertências a detenção.'
+}
