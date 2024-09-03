61fc30c4133c32d29aace1f718c30d20b8b9ab74
Bruno90028 <bruno.camacedo.2017@gmail.com>
2024-09-03 10:15:07 -0300

    atualização

--- script/atuacao.js
+++ script/atuacao.js
@@ -2,6 +2,11 @@ function civil() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Cível (ou Direito Civil):'
     descricao.innerText = 'O direito cível é uma área ampla que regula as relações entre pessoas físicas e jurídicas no dia a dia. Ele abrange uma série de subáreas, como obrigações, contratos, responsabilidade civil, direito das coisas, família e sucessões. O objetivo do direito cível é estabelecer regras para garantir que essas interações sejam justas e equilibradas, protegendo os direitos individuais e coletivos.'
@@ -12,6 +17,11 @@ function contratos() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Contratos'
     descricao.innerText = 'Essa área é especializada em acordos entre duas ou mais partes que criam obrigações legais. O direito dos contratos regula a formação, execução e rescisão desses acordos. Ele garante que as partes cumpram o que foi combinado e prevê soluções para o caso de descumprimento, como a rescisão do contrato ou indenização por danos.'
@@ -22,6 +32,11 @@ function consumidor() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Consumidor'
     descricao.innerText = 'O direito do consumidor é focado na proteção dos consumidores contra práticas abusivas por parte de fornecedores de produtos e serviços. Ele regula as relações de consumo para garantir que os direitos dos consumidores sejam respeitados, como a qualidade dos produtos, a veracidade das informações, e a segurança dos serviços prestados.'
@@ -32,6 +47,11 @@ function criminal() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Criminal'
     descricao.innerText = 'O direito criminal trata de crimes, que são ações ou omissões que a lei define como infrações penais, e das sanções aplicáveis a quem os comete. Essa área envolve o processo penal, desde a investigação policial até o julgamento e a aplicação de penas. O objetivo do direito criminal é proteger a sociedade, punir os infratores e prevenir novos crimes.'
@@ -42,6 +62,11 @@ function familia() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Família'
     descricao.innerText = 'O direito de família regula as relações familiares e aborda questões como casamento, união estável, divórcio, guarda de filhos, adoção, pensão alimentícia, e reconhecimento de paternidade. Ele busca proteger os direitos e interesses de todos os membros da família, especialmente os mais vulneráveis, como crianças e idosos.'
@@ -52,6 +77,11 @@ function imobiliario() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Imobiliário'
     descricao.innerText = 'O direito imobiliário trata das questões relacionadas a imóveis, incluindo compra e venda de propriedades, contratos de locação, usucapião (direito de adquirir um bem por posse prolongada), e regularização de escrituras. Essa área também lida com a regulamentação de condomínios e a resolução de disputas de posse ou propriedade.'
@@ -62,8 +92,88 @@ function militar() {
     var titulo = document.getElementById('ramo')
     var descricao = document.getElementById('explicacao')
     var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
 
     titulo.innerText = 'Militar'
     descricao.innerText = 'O direito militar se aplica especificamente às Forças Armadas (Exército, Marinha e Aeronáutica) e às Forças Auxiliares (Polícia Militar e Corpo de Bombeiros). Regula questões disciplinares, hierárquicas, e administrativas internas das instituições militares, além de tratar de crimes militares, que são infrações cometidas por militares em serviço ou em função da sua posição.'
     exemplo.innerText = 'Se um soldado for acusado de desobediência grave a ordens superiores durante uma missão, ele será julgado de acordo com o código de justiça militar, que pode aplicar penas que variam de advertências a detenção.'
+}
+
+function previdenciario() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
+
+    titulo.innerText = 'Previdênciário'
+    descricao.innerText = 'O direito previdenciário lida com a seguridade social, que inclui benefícios como aposentadorias, pensões, auxílios por incapacidade temporária (antigo auxílio-doença), e seguro-desemprego. O objetivo é proteger os trabalhadores e suas famílias em situações de perda de renda devido à idade, invalidez, doença ou morte.'
+    exemplo.innerText = 'Se um trabalhador se acidenta e fica incapacitado de exercer sua função por mais de 15 dias, ele pode requerer um auxílio por incapacidade temporária. Caso o benefício seja negado pelo INSS, ele pode buscar ajuda no direito previdenciário para recorrer da decisão.'
+}
+
+function sucessoes() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
+
+    titulo.innerText = 'Sucessões'
+    descricao.innerText = 'O direito das sucessões regula a transferência de bens e direitos de uma pessoa que faleceu para seus herdeiros ou beneficiários. Inclui a abertura de inventário, a partilha de bens, e a execução de testamentos. Essa área visa garantir que o patrimônio do falecido seja distribuído de acordo com a lei ou com a vontade expressa em testamento.'
+    exemplo.innerText = 'Se uma pessoa morre deixando um testamento que distribui seus bens entre filhos e uma instituição de caridade, o direito das sucessões garante que a vontade do falecido seja cumprida, resolvendo eventuais disputas entre os herdeiros.'
+}
+
+function trabalhista() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
+
+    titulo.innerText = 'Trabalhista'
+    descricao.innerText = 'O direito trabalhista regula as relações entre empregadores e empregados. Ele estabelece direitos e deveres no ambiente de trabalho, como salário, jornada de trabalho, horas extras, segurança e saúde no trabalho, e condições de rescisão contratual. Essa área visa proteger os trabalhadores de abusos e garantir condições justas de trabalho.'
+    exemplo.innerText = 'Se um trabalhador for demitido sem justa causa e a empresa se recusar a pagar as verbas rescisórias, ele pode recorrer à Justiça do Trabalho para garantir o pagamento do que lhe é devido, como aviso prévio, FGTS, e férias proporcionais.'
+}
+
+function transito() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
+
+    titulo.innerText = 'Trânsito'
+    descricao.innerText = 'O direito de trânsito abrange as leis e regulamentos que controlam a circulação de veículos e pedestres nas vias públicas. Inclui a regulamentação de infrações, penalidades, habilitação de condutores, e a responsabilidade em caso de acidentes. O objetivo é garantir a segurança e a ordem no trânsito.'
+    exemplo.innerText = 'Se um motorista é multado por ultrapassar o limite de velocidade em uma área urbana, ele pode usar o direito de trânsito para contestar a multa, caso acredite que houve erro no registro ou na aplicação da penalidade.'
+}
+
+function tributario() {
+    var titulo = document.getElementById('ramo')
+    var descricao = document.getElementById('explicacao')
+    var exemplo = document.getElementById('exemplo')
+    var inicio = document.getElementById('inicio')
+    var areas = document.getElementById('areas')
+
+    inicio.style.display = 'none'
+    areas.style.display = 'block'
+
+    titulo.innerText = 'Tributário'
+    descricao.innerText = 'O direito tributário lida com as obrigações fiscais, ou seja, os impostos, taxas e contribuições que são cobrados pelo governo de pessoas físicas e jurídicas. Ele regula a arrecadação, fiscalização e a aplicação de sanções em casos de inadimplência ou sonegação fiscal. Também protege os direitos dos contribuintes.'
+    exemplo.innerText = 'Se uma empresa recebe uma autuação fiscal por suposta sonegação de impostos, ela pode recorrer ao direito tributário para contestar a autuação e demonstrar que cumpriu com todas as obrigações fiscais.'
 }
